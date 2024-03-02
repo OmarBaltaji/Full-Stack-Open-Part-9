c320b30f658f6e92f75be6353af100ba8ff93a7e
Omar Baltaji <omr.baltaji@gmail.com>
2024-03-02 12:57:03 +0200

    9.6 Eslint

--- express/index.ts
+++ express/index.ts
@@ -5,10 +5,10 @@ const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
-})
+});
 
 app.get('/bmi', (req, res) => {
-  let { height, weight} = req.query;
+  const { height, weight} = req.query;
 
   if(isNaN(Number(height)) || isNaN(Number(weight)) || !height || !weight) {
     res.status(400).end('Malformatted  parameters');
@@ -16,10 +16,10 @@ app.get('/bmi', (req, res) => {
 
   const result = calculateBmi(Number(height), Number(weight));
   res.json({  weight, height, result });
-})
+});
 
 const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
