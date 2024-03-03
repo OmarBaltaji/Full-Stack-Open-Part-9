995e39e5c9c4f722e976f1e9d04fec40e772469b
Omar Baltaji <omr.baltaji@gmail.com>
2024-03-03 14:09:57 +0200

    9.10: Patientor backend, step3

---
+++ patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,10 @@
+import diagnoses from '../../data/diagnoses';
+import { Diagnoses } from '../types';
+
+const getAll = (): Diagnoses[] => {
+  return diagnoses;
+};
+
+export default {
+  getAll,
+};
